b33e8ebd0a07df019867f7dd1559c4c93f1b1ea1
pbg2h17ape <sven.peitzmeier_genannt_obermeie@edu.bib.de>
2019-02-14 12:31:48 +0100

    Neue Fenster erschaffen und Highscore Liste angefangen

---
+++ Development/MinesWeeber/HauptMenue.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MinesWeeber
+{
+    /// <summary>
+    /// Interaktionslogik für HauptMenue.xaml
+    /// </summary>
+    public partial class HauptMenue : Window
+    {
+        public HauptMenue()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Highscore(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void Btn_exit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_start_game(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //textfeld leeren nur zur sicherheit :)
+        }
+    }
+}
