48a69e8071739bd4b0f530e3594a5d9ad411ed3c
pbg2h17ape <sven.peitzmeier_genannt_obermeie@edu.bib.de>
2019-02-21 09:31:15 +0100

    dabei den HIghscore zumachen

--- Development/MinesWeeber/Highscore.xaml.cs
+++ Development/MinesWeeber/Highscore.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,14 +20,29 @@ namespace MinesWeeber
     /// </summary>
     public partial class Highscore : Window
     {
+       
         public Highscore()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StreamReader reader = new StreamReader("../../highscore.txt") ;
+            
+            if (reader.ReadLine() != null)
+            {
+                String line = reader.ReadLine();
+                String[] toSplit = line.Split(';');
+                libx_Scores.Items.Add(toSplit[0] + " : " + toSplit[1]);
+            }
+        }
+
         private void btn_Back_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Hide();
+            HauptMenue titelbildschirm = new HauptMenue();
+            titelbildschirm.ShowDialog();
         }
 
         private void libx_Scores_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -34,9 +50,6 @@ namespace MinesWeeber
 
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
+       
     }
 }
